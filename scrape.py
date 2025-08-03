00f0b010d24c519e40e8a00c9eea9e7d425a1c08
Varun Garg <65499846+varcodes@users.noreply.github.com>
2025-08-03 21:46:49 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -7,7 +7,7 @@ import os
 
 load_dotenv()
 
-SBR_WEBDRIVER = os.getenv("SBR_WEBDRIVER")
+SBR_WEBDRIVER = "https://brd-customer-hl_d9bbeb2e-zone-ai_scraper:61onnm4duyz7@brd.superproxy.io:9515"
 
 
 def scrape_website(website):
